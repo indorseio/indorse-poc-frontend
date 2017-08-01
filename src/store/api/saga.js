4cd4fe674d4486ee10d043672c21bdabf874f486
Kedar Vaidya <kedar@blimp.co.in>
2017-08-01 11:01:17 +0000

    Use headers for sending tokens

--- src/store/api/saga.js
+++ src/store/api/saga.js
@@ -3,22 +3,14 @@ import { call, select } from 'redux-saga/effects';
 import { callApiJson } from 'api/call';
 import * as authSelectors from 'store/auth/selectors';
 
-const preferedAuthFormat = process.env.REACT_APP_PREFERED_AUTH_FORMAT;
-
 export default function* callApi(config) {
   let finalConfig = { ...config };
 
   if (config.requireAuth) {
-    if (preferedAuthFormat === 'header') {
-      const token = yield select(authSelectors.selectAuthToken);
-      finalConfig.headers = {
-        ...finalConfig.headers,
-        'Authorization': `Bearer ${token.value}`
-      }
-    } else {
-      const token = yield select(authSelectors.selectAuthToken);
-      const currentUser = yield select(authSelectors.selectCurrentUser);
-      finalConfig.data = { ...finalConfig.data, token: token.value, email: currentUser.email };
+    const token = yield select(authSelectors.selectAuthToken);
+    finalConfig.headers = {
+      ...finalConfig.headers,
+      'Authorization': `Bearer ${token.value}`
     }
   }
 

6d340e7676a7c69c4b7627361bfa1033260055f7
Kedar Vaidya <kedar@blimp.co.in>
2017-08-03 04:55:49 +0000

    Show status in 'My Claims' and 'My Votes'

--- src/ui/claims/table/index.js
+++ src/ui/claims/table/index.js
@@ -10,12 +10,57 @@ import {
   TableRowColumn,
 } from 'material-ui/Table';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import Avatar from 'material-ui/Avatar';
+import FontIcon from 'material-ui/FontIcon';
+import Chip from 'material-ui/Chip';
+import autoBind from 'react-autobind';
 
 import routeTemplates from 'ui/common/routes/templates';
+import themeConfig from 'ui/theme/config';
+import messages from 'ui/claims/messages';
 
 class ClaimsTable extends Component {
   static propTypes = {
     claims: PropTypes.array
+  }
+
+  constructor(props) {
+    super(props);
+
+    autoBind(this);
+  }
+
+  renderStatus(claim) {
+    const { intl: { formatMessage } } = this.props;
+    const { votingRound } = claim;
+
+    if (moment().isBefore(moment(votingRound.endVoting))) {
+        return (<Chip backgroundColor={themeConfig.palette.info} labelColor={themeConfig.palette.white}>
+          <Avatar
+            backgroundColor={themeConfig.palette.info}
+            color={themeConfig.palette.white}
+            icon={<FontIcon className="material-icons">timer</FontIcon>} />
+          {formatMessage(messages.pending)}
+        </Chip>);
+      } else if (claim.result) {
+        const endorsed = claim.result === 'endorsed';
+        const color = endorsed ? themeConfig.palette.success : themeConfig.color.danger;
+        const icon = endorsed ? 'check circle' : 'close';
+        const text = endorsed ? 'Endorsed' : 'Flagged';
+        return (<Chip backgroundColor={color} labelColor="#ffffff">
+          <Avatar backgroundColor={color} color="#ffffff" icon={<FontIcon className="material-icons">{icon}</FontIcon>} />
+          {text}
+        </Chip>);
+      } else {
+        return (<Chip backgroundColor={themeConfig.palette.warning} labelColor={themeConfig.palette.white}>
+          <Avatar
+            backgroundColor={themeConfig.palette.warning}
+            color={themeConfig.palette.white}
+            icon={<FontIcon className="material-icons">warning</FontIcon>} />
+          {formatMessage(messages.unverified)}
+        </Chip>);
+      }
   }
 
   render() {
@@ -31,6 +76,9 @@ class ClaimsTable extends Component {
             <TableHeaderColumn>
               <FormattedMessage id="claim.proof" defaultMessage="Proof" />
             </TableHeaderColumn>
+            <TableHeaderColumn>
+              <FormattedMessage id="claim.status" defaultMessage="Status" />
+            </TableHeaderColumn>
           </TableRow>
         </TableHeader>
 
@@ -42,6 +90,9 @@ class ClaimsTable extends Component {
               </TableRowColumn>
               <TableRowColumn>
                 <div className="lead">{claim.proof}</div>
+              </TableRowColumn>
+              <TableRowColumn>
+                {this.renderStatus(claim)}
               </TableRowColumn>
             </TableRow>
           ))}
--- src/ui/votes/table/index.js
+++ src/ui/votes/table/index.js
@@ -9,27 +9,108 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import moment from 'moment';
 import { Link } from 'react-router-dom';
+import autoBind from 'react-autobind';
+import Avatar from 'material-ui/Avatar';
+import FontIcon from 'material-ui/FontIcon';
+import Chip from 'material-ui/Chip';
 
 import routeTemplates from 'ui/common/routes/templates';
+import themeConfig from 'ui/theme/config';
+import votesMessages from 'ui/votes/messages';
+import claimMessages from 'ui/claims/messages';
 
 class VotesTable extends Component {
   static propTypes = {
     votes: PropTypes.array
   }
 
+  constructor(props) {
+    super(props);
+
+    autoBind(this);
+  }
+
+  renderResult(vote) {
+    const { votingRound, claim } = vote;
+    const { intl: { formatMessage } } = this.props;
+
+    if (moment().isBefore(moment(votingRound.endVoting))) {
+      return (<Chip backgroundColor={themeConfig.palette.info} labelColor={themeConfig.palette.white}>
+        <Avatar
+          backgroundColor={themeConfig.palette.info}
+          color={themeConfig.palette.white}
+          icon={<FontIcon className="material-icons">timer</FontIcon>} />
+        {formatMessage(claimMessages.pending)}
+      </Chip>);
+    } else if (claim.result) {
+      const endorsed = claim.result === 'endorsed';
+      const color = endorsed ? themeConfig.palette.success : themeConfig.color.danger;
+      const icon = endorsed ? 'check circle' : 'close';
+      const text = endorsed ? 'Endorsed' : 'Flagged';
+      return (<Chip backgroundColor={color} labelColor="#ffffff">
+        <Avatar backgroundColor={color} color="#ffffff" icon={<FontIcon className="material-icons">{icon}</FontIcon>} />
+        {text}
+      </Chip>);
+    } else {
+      return (<Chip backgroundColor={themeConfig.palette.warning} labelColor={themeConfig.palette.white}>
+        <Avatar
+          backgroundColor={themeConfig.palette.warning}
+          color={themeConfig.palette.white}
+          icon={<FontIcon className="material-icons">warning</FontIcon>} />
+        {formatMessage(claimMessages.unverified)}
+      </Chip>);
+    }
+  }
+
+  renderVoteStatus(vote) {
+    const { intl: { formatMessage } } = this.props;
+
+    if (vote.registered) {
+      if (vote.votedAt) {
+        return vote.endorsed ?
+          <Chip
+            backgroundColor={themeConfig.palette.success}
+            labelColor={themeConfig.palette.white}>
+            <Avatar
+              backgroundColor={themeConfig.palette.success}
+              color={themeConfig.palette.white}
+              icon={<FontIcon className="material-icons">check circle</FontIcon>} />
+            {formatMessage(votesMessages.endorsed)}
+          </Chip> :
+          <Chip
+            backgroundColor={themeConfig.palette.danger}
+            labelColor={themeConfig.palette.white}>
+            <Avatar
+              backgroundColor={themeConfig.palette.danger}
+              color={themeConfig.palette.white}
+              icon={<FontIcon className="material-icons">close</FontIcon>} />
+            {formatMessage(votesMessages.flagged)}
+          </Chip>
+      }
+    }
+  }
+
+
   render() {
     const { votes } = this.props;
 
     return (
-      <Table>
+      <Table className="my-3">
         <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
           <TableRow>
             <TableHeaderColumn>
-              <FormattedMessage id="claim.title" defaultMessage="Title" />
+              <FormattedMessage id="vote.title" defaultMessage="Title" />
             </TableHeaderColumn>
             <TableHeaderColumn>
-              <FormattedMessage id="claim.proof" defaultMessage="Proof" />
+              <FormattedMessage id="vote.proof" defaultMessage="Proof" />
+            </TableHeaderColumn>
+            <TableHeaderColumn>
+              <FormattedMessage id="vote.claim.result" defaultMessage="Result" />
+            </TableHeaderColumn>
+            <TableHeaderColumn>
+              <FormattedMessage id="vote.your" defaultMessage="Your Vote" />
             </TableHeaderColumn>
           </TableRow>
         </TableHeader>
@@ -42,6 +123,12 @@ class VotesTable extends Component {
               </TableRowColumn>
               <TableRowColumn>
                 <div className="lead">{vote.claim.proof}</div>
+              </TableRowColumn>
+              <TableRowColumn>
+                {this.renderResult(vote)}
+              </TableRowColumn>
+              <TableRowColumn>
+                {this.renderVoteStatus(vote)}
               </TableRowColumn>
             </TableRow>
           ))}

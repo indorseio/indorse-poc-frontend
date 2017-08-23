3e9f753485b581f1df9f0cff8b1d452e3381abf3
Kedar Vaidya <kedar@blimp.co.in>
2017-08-23 11:34:34 +0000

    Fix sentry DSN

--- src/install-raven.js
+++ src/install-raven.js
@@ -1,7 +1,7 @@
 import Raven from 'raven-js';
 
 export default function installRaven() {
-  const ravenDSN = `https://${process.env.REACT_APP_SENTRY_KEY}.io/${process.env.REACT_APP_SENTRY_PROJECT}`;
+  const ravenDSN = `https://${process.env.REACT_APP_SENTRY_KEY}@sentry.io/${process.env.REACT_APP_SENTRY_PROJECT}`;
   Raven.config(ravenDSN).install();
   return Raven;
 }
